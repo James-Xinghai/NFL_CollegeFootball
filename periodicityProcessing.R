b6a04c3e4f497895450a1177ae014c7b4b9d9f4a
James (Xinghai) <xinghai@inchfriend-lm.corp.yahoo.com>
2014-08-05 09:42:01 -0700

    add R script for data processing

---
+++ periodicityProcessing.R
@@ -0,0 +1,20 @@
+setwd('/Users/xinghai/Documents/Yahoo/NFL_collegeFootball/code/NFL_CollegeFootball')
+
+spaceID = '95861059'
+filePath = paste('../../', spaceID, 'New.csv', sep = "")
+mydata = read.csv(filePath)
+
+monthlist <- rep(12, 0)
+weeklist  <- rep(7, 0)
+
+for (i in 1:12) {
+  monthlist[i] <- mean(mydata[which(mydata[, 2] == i) ,5])
+}
+
+for (i in 0:6) {
+  weeklist[i+1] <- mean(mydata[which(mydata[, 4] == i) ,5])
+}
+
+
+
+
